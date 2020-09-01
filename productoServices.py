41ead5607cf2f39138b43e8645e967f6c5255592
militomba <mariamilagrostomba@gmail.com>
2020-09-01 11:40:29 -0300

    trabajo 6

---
+++ productoServices.py
@@ -0,0 +1,70 @@
+from producto import Producto
+from repository import Repositorios
+
+
+class ProductoService():
+
+    def get_productosList(self):
+        print("Listar Productos")
+        return Repositorios.productosList
+
+    def crearProducto(self):
+        print("\n---CREAR PRODUCTO")
+        descripcion = input("ingrese la descripcion del producto: ")
+        precio = int(input("ingrese el precio: "))
+        tipo = input("ingrese el tipo de libro: ")
+        return Producto(descripcion, precio, tipo)
+
+    '#AGREGAR PRODUCTO'
+    def add_producto(self, producto=None):
+        print("\n----AGREGAR PRODUCTO")
+        if producto is None:
+            producto = self.crearProducto
+        lastKey = -1
+        for productoKey in Repositorios.productosList:
+            lastKey = productoKey
+        lastKey = int(lastKey) + 1
+        Repositorios.productosList[lastKey] = producto.__dict__
+        return lastKey
+
+    '#ACTUALIZAR PRODUCTO'
+    def update_producto(self, productoKey=None, producto=None):
+        print("\n----MODIFICAR PERSONA")
+        if productoKey is None:
+            productoKey = int(input("Ingrese una llave: "))
+        if producto is None:
+            producto = self.crearProducto
+        Repositorios.productosList[productoKey] = producto.__dict__
+
+    '#algoritmo insertion'
+    def insertion_sort_precio(self, lista, tipo_orden):
+        lista_ordenada = lista.copy()
+        if tipo_orden == "ascendente":
+            for i in range(0, len(lista_ordenada)):
+                valorNuevo = lista_ordenada[i]
+                posicion = i - 1
+                while posicion >= 0 and \
+                    lista_ordenada[posicion]["_precio"] > \
+                        valorNuevo["_precio"]:
+                    lista_ordenada[posicion + 1] = lista_ordenada[posicion]
+                    posicion = posicion - 1
+                lista_ordenada[posicion + 1] = valorNuevo
+
+        if tipo_orden == "descendente":
+            for i in range(0, len(lista_ordenada)):
+                valorNuevo = lista_ordenada[i]
+                posicion = i - 1
+                while posicion >= 0 and \
+                    lista_ordenada[posicion]["_precio"] < \
+                        valorNuevo["_precio"]:
+                    lista_ordenada[posicion + 1] = lista_ordenada[posicion]
+                    posicion = posicion - 1
+                lista_ordenada[posicion + 1] = valorNuevo
+        return lista_ordenada
+
+    '#ELIMINAR PRODUCTO'
+    def delete_producto(self, productoKey=None):
+        print("\n----ELIMINAR PERSONA")
+        if productoKey not in Repositorios.productosList:
+            raise ValueError("La llave no existe")
+        del Repositorios.productosList[productoKey]
